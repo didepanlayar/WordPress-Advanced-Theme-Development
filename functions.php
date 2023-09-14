6bb4edc04c05c267b20551afd725e3a32719c8b4
Di Depan Layar <albinmaulana@yahoo.com>
2023-09-14 22:18:03 +0700

    Refactor: Enqueue Stylesheet to Bypass Cache

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function get_enqueue() {
-    wp_enqueue_style('stylesheet', get_stylesheet_uri(), array(), false);
+    wp_enqueue_style('stylesheet', get_stylesheet_uri(), array(), fileatime(get_template_directory() . '/style.css'), 'all');
     wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap', array(), null);
 }
 add_action('wp_enqueue_scripts', 'get_enqueue');
