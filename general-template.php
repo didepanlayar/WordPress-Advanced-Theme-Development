d9e4c0e2de5e60546f3a8c79f8ad790c97fd0d84
Di Depan Layar <albinmaulana@yahoo.com>
2023-09-15 20:54:14 +0700

    Feature: Add General Template Page

---
+++ general-template.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Template Name: General Template
+*/
+?>
+
+<?php get_header(); ?>
+        <!--  -->
+        <div id="content" class="site-content">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main">
+                    <div class="container">
+                        <div class="general-template">
+                            <?php if(have_posts()) : ?>
+                                <?php while(have_posts()) : the_post(); ?>
+                                    <article>
+                                        <h1><?php the_title(); ?></h1>
+                                        <?php the_content(); ?>
+                                    </article>
+                                <?php endwhile; ?>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </div>
+<?php get_footer(); ?>
