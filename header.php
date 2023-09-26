62cce3b5c657123413cba448b13bbbddd02ec596
Di Depan Layar <albinmaulana@yahoo.com>
2023-09-26 11:56:34 +0700

    Feature: Conditional Tags in Pages

--- header.php
+++ header.php
@@ -21,25 +21,27 @@
                     </div>
                 </div>
             </section>
-            <section class="menu-area">
-                <div class="container">
-                    <nav class="main-menu">
-                        <button class="check-button">
-                            <div class="menu-icon">
-                                <div class="bar1"></div>
-                                <div class="bar2"></div>
-                                <div class="bar3"></div>
-                            </div>
-                        </button>
-                        <?php
-                            wp_nav_menu(
-                                array(
-                                    'theme_location'        => 'wordev_main_menu',
-                                    'depth'                 => 2
-                                )
-                            );
-                        ?>
-                    </nav>
-                </div>
-            </section>
+            <?php if(! is_page('landing-page')) : ?>
+                <section class="menu-area">
+                    <div class="container">
+                        <nav class="main-menu">
+                            <button class="check-button">
+                                <div class="menu-icon">
+                                    <div class="bar1"></div>
+                                    <div class="bar2"></div>
+                                    <div class="bar3"></div>
+                                </div>
+                            </button>
+                            <?php
+                                wp_nav_menu(
+                                    array(
+                                        'theme_location'        => 'wordev_main_menu',
+                                        'depth'                 => 2
+                                    )
+                                );
+                            ?>
+                        </nav>
+                    </div>
+                </section>
+            <?php endif; ?>
         </header>
