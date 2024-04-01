0bc66888ab62e9405c1abb39a471739065db413a
Di Depan Layar <albinmaulana@yahoo.com>
2024-04-01 20:17:54 +0700

    Feature: Block Editor and Gutenberg Project Part 4

--- functions.php
+++ functions.php
@@ -47,24 +47,24 @@ function wordev_config() {
         'script'
     ));
     add_theme_support('title-tag');
-    add_theme_support('align-wide');
+    // add_theme_support('align-wide');
     add_theme_support('responsive-embeds');
     add_theme_support('editor-styles');
     add_editor_style('style-editor.css');
     add_theme_support('wp-block-styles');
-    add_theme_support('editor-color-palette', array(
-        array(
-            'name'  => __('Primary', 'wordev'),
-            'slug'  => 'primary',
-            'color' => '#001E32'
-        ),
-        array(
-            'name'  => __('Secondary', 'wordev'),
-            'slug'  => 'secondary',
-            'color' => '#CFAF07'
-        )
-    ));
-    add_theme_support('disable-custom-colors');
+    // add_theme_support('editor-color-palette', array(
+    //     array(
+    //         'name'  => __('Primary', 'wordev'),
+    //         'slug'  => 'primary',
+    //         'color' => '#001E32'
+    //     ),
+    //     array(
+    //         'name'  => __('Secondary', 'wordev'),
+    //         'slug'  => 'secondary',
+    //         'color' => '#CFAF07'
+    //     )
+    // ));
+    // add_theme_support('disable-custom-colors');
 }
 add_action('after_setup_theme', 'wordev_config', 0);
 
